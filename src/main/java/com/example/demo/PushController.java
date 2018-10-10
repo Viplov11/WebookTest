7e6bfbb2a15ad9b8152321b1c406952bbcdfcb07
Viplov Patney <viplov.patney@transferwise.com>
2018-10-10 13:06:52 +0100

    Changing request mapping

--- src/main/java/com/example/demo/PushController.java
+++ src/main/java/com/example/demo/PushController.java
@@ -3,14 +3,16 @@ package com.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
-@Controller("/api/v1/one-time-token")
+@RestController
+@RequestMapping("/api/v1/one-time-token")
 public class PushController {
 
 	Logger logger = LoggerFactory.getLogger(PushController.class);
